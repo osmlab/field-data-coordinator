b0815f99713f9b4747471615f07e7e4550ed1c84
Derek Lieu <dl@dereklieu.com>
2017-06-05 15:43:55 +0100

    Include material ui and roboto font library

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,7 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Home = require('./ui/Home.jsx')
 
+// https://github.com/callemall/material-ui#react-tap-event-plugin
+require('react-tap-event-plugin')()
+
 ReactDOM.render(<Home />, document.getElementById('root'))

281be7230821cefe45879700d1025237e5c8b508
Derek Lieu <dl@dereklieu.com>
2017-06-06 20:18:59 +0100

    Include mapbox gl

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,14 @@
 'use strict'
 const React = require('react')
 const Header = require('./Header.jsx')
+const Map = require('./Map.jsx')
 
 module.exports = class App extends React.Component {
   render () {
     return (
       <div>
         <Header />
-        <h1>Hello world!</h1>
+        <Map />
       </div>
     )
   }
---
+++ src/ui/Map.jsx
@@ -0,0 +1,29 @@
+'use strict'
+const React = require('react')
+const mapboxgl = require('mapbox-gl')
+const { connect } = require('react-redux')
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFwZWd5cHQiLCJhIjoiY2l6ZTk5YTNxMjV3czMzdGU5ZXNhNzdraSJ9.HPI_4OulrnpD8qI57P12tg'
+
+class Map extends React.Component {
+  constructor (props) {
+    super(props)
+    this.init = this.init.bind(this)
+  }
+
+  init (el) {
+    this.map = new mapboxgl.Map({
+      container: el,
+      style: 'mapbox://styles/mapbox/satellite-v9'
+    })
+  }
+
+  render () {
+    return (
+      <div className='map' ref={this.init} />
+    )
+  }
+}
+
+const mapStateToProps = state => ({ observations: state.observations })
+module.exports = connect(mapStateToProps)(Map)

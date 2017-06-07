ecfadf55530420498553e1c8790e88db4665ac28
Derek Lieu <dl@dereklieu.com>
2017-06-07 12:30:36 +0100

    Don't actually need redux-immutable

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
 'use strict'
-const { combineReducers } = require('redux-immutable')
+const { combineReducers } = require('redux')
 const observations = require('./observations')
 module.exports = combineReducers({ observations })

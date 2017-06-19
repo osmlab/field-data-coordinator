1962dd868918cb298a0c0cbb1f31c59519a090f2
Derek Lieu <dl@dereklieu.com>
2017-06-19 18:43:37 -0400

    Create an export button

--- src/ui/data/index.jsx
+++ src/ui/data/index.jsx
@@ -1,7 +1,8 @@
 'use strict'
 const React = require('react')
-const Observations = require('./observations.jsx')
-const Observation = require('./observation.jsx')
+const Observations = require('./Observations.jsx')
+const Observation = require('./Observation.jsx')
+const Export = require('./Export.jsx')
 const { Route } = require('react-router-dom')
 
 module.exports = class Data extends React.Component {
@@ -9,6 +10,7 @@ module.exports = class Data extends React.Component {
     const { match } = this.props
     return (
       <div>
+        <Export />
         <Route path={`${match.url}/observation/:observationId`} component={Observation} />
         <Route exact path={match.url} component={Observations} />
       </div>

8ac3aea94f31a90a3023fd7408cc605094761935
Derek Lieu <dl@dereklieu.com>
2017-06-23 14:37:26 -0400

    Create action for querying OSM

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,15 @@ module.exports.importSurvey = () => ({
   type: 'IMPORT_SURVEY'
 })
 
+module.exports.updateSurveyList = surveys => {
+  return {
+    type: 'RECEIVE_SURVEY_LIST',
+    surveys
+  }
+}
+
 module.exports.sync = () => ({ type: 'SYNC' })
+module.exports.getOSM = (bounds) => ({ type: 'GET_OSM', bounds })
 
 module.exports.toggleFilterProperty = property => ({
   type: 'TOGGLE_FILTER_PROPERTY',
@@ -17,10 +25,3 @@ module.exports.setActiveObservation = (observationId) => ({
   type: 'SET_ACTIVE_OBSERVATION',
   observationId
 })
-
-module.exports.updateSurveyList = surveys => {
-  return {
-    type: 'RECEIVE_SURVEY_LIST',
-    surveys
-  }
-}

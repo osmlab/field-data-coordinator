448a005e38a14e92a6a30e36d3d4efc728cb40bc
Derek Lieu <dl@dereklieu.com>
2017-06-29 10:57:44 -0400

    Remove unnecessary mapnik rebuild

--- bin/rebuild-mapnik.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-'use strict'
-
-var exec = require('npm-execspawn')
-
-var child = exec('cd node_modules/mapnik && node-gyp rebuild --target=1.7.3 --runtime=electron --arch=x64 --dist-url=https://atom.io/download/electron', { env: { HOME: '~/.electron-gyp' } })
-child.stderr.pipe(process.stderr)
-child.stdout.pipe(process.stdout)

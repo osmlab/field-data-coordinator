f52e33829d5207f747acf5973eb275afcdf12c82
Derek Lieu <dl@dereklieu.com>
2017-06-28 20:51:06 -0400

    Spell out config with build info

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-module.exports.osmapi = 'http://api.openstreetmap.org/api/0.6/'
+const path = require('path')
+const config = require('./package.json')
+
+module.exports = {
+  osmapi: 'http://api.openstreetmap.org/api/0.6/',
+  appName: 'Observe',
+  appFileName: 'observe-desktop',
+  version: config.version,
+  iconPath: path.join(__dirname, 'static'),
+  out: 'release',
+  electronVersion: '1.7.3'
+}

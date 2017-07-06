567e0c321b321462c031d4d27fd8ada3811aa071
Derek Lieu <dl@dereklieu.com>
2017-07-06 18:58:10 -0400

    Create rudimentary dropdown for exports

--- src/ui/data/Export.jsx
+++ src/ui/data/Export.jsx
@@ -1,33 +1,66 @@
 'use strict'
 const React = require('react')
 const { connect } = require('react-redux')
-const { getActiveFeatures } = require('../../reducers/observations')
 
 class Export extends React.Component {
   constructor (props) {
     super(props)
-    this.exportActive = this.exportActive.bind(this)
+    this.exportXml = this.exportXml.bind(this)
+    this.exportJson = this.exportJson.bind(this)
+    this.exportGeojson = this.exportGeojson.bind(this)
+    this.toggleOptions = this.toggleOptions.bind(this)
+    this.state = {
+      showOptions: false
+    }
   }
 
-  exportActive () {
-    const { activeIds, activeFeatures } = this.props
-    console.log(activeIds.toJS())
-    console.log(activeFeatures)
+  toggleOptions () {
+    const { showOptions } = this.state
+    if (!showOptions) {
+      // delay the listener to avoid wiping the initial click
+      window.setTimeout(() => {
+        window.addEventListener('click', this.toggleOptions)
+      }, 0)
+    } else {
+      window.removeEventListener('click', this.toggleOptions)
+    }
+    this.setState({ showOptions: !showOptions })
+  }
+
+  getActive () {
+    return this.props.activeIds.toJS()
+  }
+
+  exportXml () {
+  }
+
+  exportJson () {
+  }
+
+  exportGeojson () {
   }
 
   render () {
     return (
       <div className='export'>
-        <button
-          className='button'
-          onClick={this.exportActive}>Export Selected Data</button>
+        <div className='dropdown'>
+          <button
+            className='button'
+            onClick={this.toggleOptions}>Export Selected Data</button>
+          { this.state.showOptions ? (
+            <ul className='dropdownOptions'>
+              <li className='dropdownOption' onClick={this.exportXml}>XML</li>
+              <li className='dropdownOption' onClick={this.exportJson}>JSON</li>
+              <li className='dropdownOption' onClick={this.exportGeojson}>GeoJSON</li>
+            </ul>
+          ) : null }
+        </div>
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  activeIds: state.observations.get('active'),
-  activeFeatures: getActiveFeatures(state.observations)
+  activeIds: state.observations.get('active')
 })
 module.exports = connect(mapStateToProps)(Export)

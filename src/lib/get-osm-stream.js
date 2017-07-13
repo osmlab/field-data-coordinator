d34a6931c621acac1cf772d011b4ef96c7b8ccbb
Derek Lieu <dl@dereklieu.com>
2017-07-13 15:14:44 -0400

    Add logging

--- src/lib/get-osm-stream.js
+++ src/lib/get-osm-stream.js
@@ -6,8 +6,10 @@ const { osmapi } = require('../config')
 // Query the osm api using a bounding box string, returns a readable stream
 module.exports = function getOsmStream (bbox, cb) {
   const queryUrl = url.resolve(osmapi, `map?bbox=${bbox}`)
+  console.log('fetching', queryUrl)
   http.get(queryUrl, (res) => {
     const { statusCode } = res
+    console.log('received response with code', statusCode)
     if (statusCode >= 400) {
       var body = ''
       res.on('data', (chunk) => {

60ada69181d7115e0c4bdf23285f0c3845b73716
Anna Scalamogna <anna@developmentseed.org>
2017-07-20 11:56:13 -0400

    add styles to observation point page

--- src/ui/data/Metadata.jsx
+++ src/ui/data/Metadata.jsx
@@ -3,10 +3,29 @@ const React = require('react')
 class Metadata extends React.Component {
   render () {
     return (
-      <aside role='complementary' className='sidebar sidebar__metadata'>
-        <h2>Basic Information</h2>
-        <p>Name of point</p>
-        <p>TODO</p>
+      <aside role='complementary' className='sidebar--observations'>
+        <div className='meta__group'>
+          <h2 className='data__subtitle'>Basic Information <span><a className='link--primary link--edit'>Edit</a></span></h2>
+          <dl>
+            <dt className='data__tag'>Name of point</dt>
+            <dd className='data__tag-def'>Cheboygan Area State Part</dd>
+            <dt className='data__tag'>Address</dt>
+            <dd className='data__tag-def'>124 Cheboygan Dr. Cheboygan MI, 49721</dd>
+          </dl>
+        </div>
+        <div className='meta__group'>
+          <h2 className='data__subtitle'>Notes</h2>
+          <p className='meta__prose'>Add anything that is happening at this point in time that you think is valuable to record.</p>
+          <a className='link--primary'>+ Add a New Note</a>
+        </div>
+        <div className='meta__group'>
+          <h2 className='data__subtitle'>Tags <span><a className='link--primary link--edit'>Edit</a></span></h2>
+          <ul className='meta__prose'>
+            <li>Natural/Oil Spill</li>
+            <li>Hazard</li>
+          </ul>
+          <a className='link--primary'> + Add a New Tag</a>
+        </div>
       </aside>
     )
   }
--- src/ui/home/index.jsx
+++ src/ui/home/index.jsx
@@ -12,19 +12,19 @@ module.exports = class App extends React.Component {
               <div className='card__image'></div>
               <h2 className='card__title'>Create Surveys</h2>
               <p className='card__prose'>Create surveys using our formatting in google docs or xcel. Import   yourspreadsheet   and share the link withyour surveyors</p>
-              <a className='card__link primary--link'>Create a Survey</a>
+              <a className='card__link link--primary'>Create a Survey</a>
             </div>
             <div className='header__card'>
               <div className='card__image'></div>
               <h2 className='card__title'>Collect Surveys</h2>
               <p className='card__prose'>Surveyors can edit OSM information or complete surveys through their andriod devices .</p>
-              <a className='card__link primary--link'>Download the App</a>
+              <a className='card__link link--primary'>Download the App</a>
             </div>
             <div className='header__card'>
               <div className='card__image'></div>
               <h2 className='card__title'>Edit Data</h2>
               <p className='card__prose'>Collected data will sync back to the coordinator app, allowing coordinators to   clean up the data.</p>
-              <a className='card__link primary--link'>View Collected Data</a>
+              <a className='card__link link--primary'>View Collected Data</a>
             </div>
             <div className='header__card'>
               <div className='card__image'></div>
@@ -45,17 +45,17 @@ module.exports = class App extends React.Component {
                   <li className='data__item'>49° N 100° E</li>
                   <li className='data__item'>Category</li>
                 </ul>
-                <dl>
-                  <dt>Author:</dt>
-                  <dd>Author Name</dd>
-                  <dt>Date:</dt>
-                  <dd>2/26/17</dd>
+                <dl className='meta-card__list'>
+                  <dt className='meta-card__title'>Author:</dt>
+                  <dd className='meta-card__def'>Author Name</dd>
+                  <dt className='meta-card__title'>Date:</dt>
+                  <dd className='meta-card__def'>2/26/17</dd>
                 </dl>
                 <p className='data_item'>80% complete</p>
               </div>
             </div>
           </div>
-          <a className='page--link primary--link'>View All Data Points</a>
+          <a className='link--page link--primary'>View All Data Points</a>
         </section>
       </div>
     )

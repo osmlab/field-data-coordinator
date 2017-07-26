f7cf96ae63b42f3d166af859ed1c8bac5bc2005f
Derek Lieu <dl@dereklieu.com>
2017-07-26 15:32:06 -0400

    Create remove survey method

--- src/drivers/local.js
+++ src/drivers/local.js
@@ -16,12 +16,15 @@ const {
   openExportShapefileDialog
 } = remote.require('./main')
 
+const { removeSurvey } = remote.require('./lib/surveys')
+
 // Use these drivers to interface with a local osm p2p instance.
 // This should *in theory* make it easier to create a hosted version.
 
 module.exports = {
   listObservations: promisify(listObservations),
   importSurvey: promisify(openImportSurveyDialog),
+  removeSurvey: promisify(removeSurvey),
   importOsm: promisify(importBulkOsm),
   querySavedOsm: promisify(bboxQuerySavedOsm),
 

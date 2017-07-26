74945907d9704f0d7403a1a30e53a789bc9c7c69
Stephen Whitmore <sww@eight45.net>
2017-07-26 10:08:26 -0700

    only apply app subdir if not present already

--- src/lib/app-path.js
+++ src/lib/app-path.js
@@ -5,8 +5,16 @@ const { app } = require('electron')
 const APP_DIR = 'org.osm-labs.field-data-coordinator'
 
 module.exports = function () {
-  var dir = path.join(app.getPath('userData'), APP_DIR)
+  var dir = app.getPath('userData')
+
+  // Development mode doesn't include the APP_DIR subdirectory, but production
+  // does.
+  if (dir.indexOf(APP_DIR) !== -1) {
+    dir = path.join(dir, APP_DIR)
+  }
+
   mkdirp.sync(dir)
+
   return dir
 }
 

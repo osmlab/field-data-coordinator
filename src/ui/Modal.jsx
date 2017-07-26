775de1c69439b4e451a2dc55e4efd3cad67ef006
Derek Lieu <dl@dereklieu.com>
2017-07-26 14:21:54 -0400

    Allow modal to take a classname

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,13 +1,17 @@
 'use strict'
+const PropTypes = require('prop-types')
 const React = require('react')
 class Modal extends React.Component {
   render () {
     return (
-      <div>
+      <div className={this.props.className}>
         <div className='modal' />
         <div className='modal__inner'>{this.props.children}</div>
       </div>
     )
   }
+}
+Modal.PropTypes = {
+  className: PropTypes.string
 }
 module.exports = Modal

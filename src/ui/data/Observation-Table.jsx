02a65262813ce6e1a50999458cc956acc186e3e1
Derek Lieu <dl@dereklieu.com>
2017-08-09 16:42:41 -0400

    Allow table sorting

--- src/ui/data/Observation-Table.jsx
+++ src/ui/data/Observation-Table.jsx
@@ -1,6 +1,7 @@
 'use strict'
 const React = require('react')
 const Paginator = require('paginator')
+const get = require('object-path').get
 const { getActiveFeatures } = require('../../selectors')
 const { connect } = require('react-redux')
 const nullvalue = '--'
@@ -19,14 +20,45 @@ class ObservationTable extends React.Component {
   constructor (props) {
     super(props)
     this.renderPagination = this.renderPagination.bind(this)
+    this.setSortProperty = this.setSortProperty.bind(this)
+    this.getColumnNames = this.getColumnNames.bind(this)
+    const columnNames = this.getColumnNames()
     this.state = {
-      page: 1
+      page: 1,
+      sortProperty: columnNames[0],
+      sortOrder: 1,
+      columnNames
     }
+  }
+
+  componentWillReceiveProps ({ activeFeatureIds }) {
+    // On new active feature list
+    if (this.props.activeFeatureIds !== activeFeatureIds) {
+      this.setState({ page: 1, columnNames: this.getColumnNames() })
+    }
+  }
+
+  getColumnNames () {
+    // Create the world of properties to use as column headers
+    const features = get(this.props, 'activeFeatures.features', [])
+    const properties = {}
+    features.forEach(feature => {
+      for (let property in feature.properties) {
+        properties[property] = true
+      }
+    })
+    return Object.keys(properties)
+  }
+
+  setSortProperty (prop) {
+    const { sortProperty, sortOrder } = this.state
+    if (prop === sortProperty) this.setState({ sortOrder: sortOrder * -1 })
+    else this.setState({ sortProperty: prop, sortOrder: 1 })
   }
 
   renderPagination () {
     const currentPage = this.state.page
-    const count = this.props.activeFeatures.features.length
+    const count = get(this.props, 'activeFeatures.features', []).length
     const paginator = new Paginator(limit, 7)
     const meta = paginator.build(count, currentPage)
     const pages = []
@@ -48,38 +80,39 @@ class ObservationTable extends React.Component {
     )
   }
 
-  componentWillReceiveProps ({ activeFeatureIds }) {
-    // new active feature list
-    if (this.props.activeFeatureIds !== activeFeatureIds) {
-      this.setState({ page: 1 })
-    }
-  }
-
   render () {
-    const { activeFeatures } = this.props
-    const features = activeFeatures.features
-    const properties = {}
-    features.forEach(feature => {
-      for (let property in feature.properties) {
-        properties[property] = true
-      }
-    })
-    const columnNames = Object.keys(properties)
+    const { page, sortOrder, sortProperty, columnNames } = this.state
+    const features = get(this.props, 'activeFeatures.features', [])
+
+    // Determine if we need to paginate
     const showPagination = features.length > limit
-    const stops = showPagination ? getPaginatedStops(features, this.state.page)
+    const { start, stop } = showPagination ? getPaginatedStops(features, page)
       : { start: 0, stop: features.length }
+
+    // Determine the sort order
+    const prop = sortProperty || columnNames[0]
+    const sortFunction = (a, b) => a.properties[prop] > b.properties[prop] ? -1 : 1
+
+    // Get the sorted, paginated features
+    const sortedRows = features.sort(sortFunction)
+    if (sortOrder < 0) sortedRows.reverse()
+    const visibleRows = sortedRows.slice(start, stop)
+
     return (
       <div className='tableScrollContainer'>
         { showPagination ? this.renderPagination() : null }
-        <p>Showing {stops.start + 1} - {stops.stop}</p>
+        <p>Showing {start + 1} - {stop}</p>
         <table className='table'>
           <thead>
             <tr>
-              {columnNames.map(n => <th key={n}>{n}</th>)}
+              {columnNames.map(n => (
+                <th key={n}
+                  onClick={() => this.setSortProperty(n)}>{n}</th>
+              ))}
             </tr>
           </thead>
           <tbody>
-            {features.slice(stops.start, stops.stop).map(feature => (
+            {visibleRows.map(feature => (
               <tr key={feature.id}>
                 {columnNames.map(n => (
                   <td key={feature.id + n}>{feature.properties[n] || nullvalue}</td>

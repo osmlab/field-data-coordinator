f93510190784db6f511105cd84ea9e2fd4cb2733
Derek Lieu <dl@dereklieu.com>
2017-09-28 11:07:53 -0400

    Add external link feature

--- src/ui/about/index.jsx
+++ src/ui/about/index.jsx
@@ -1,37 +1,28 @@
 'use strict'
 const React = require('react')
-const get = require('object-path').get
-const { Link } = require('react-router-dom')
-const { connect } = require('react-redux')
-const { getRecentObservations } = require('../../selectors')
-const PropTypes = require('prop-types')
-const { nullValue, date, coordinates } = require('../format')
-const { SOURCE, markerStyle } = require('../map/config')
-const { version, surveyType, device, timestamp } = require('../data/property-names').accessors
-
+const openExternalLink = require('../external-link')
 
 class About extends React.Component {
-
   render () {
     return (
-      <div className="body__content">
+      <div className='body__content'>
         <section className='row initialSection section__about'>
           <p>The Observe Desktop App is the hub for mappers to sync surveys, share observations, and export data to OSM. It allows a field manager to manage the surveys that are synced to everyone's phones, as well as examine and filter the observations that mappers make. As a desktop app, it supports both MacOS and Windows environments. Finally, it automatically syncs surveys and observations, once it detects mobile phones that have Observe Mobile on the same wifi connection.</p>
         </section>
         <section className='row section__about'>
           <h3>Getting Started</h3>
-          <p>The Observe App comes pre-filled with the standard OSM survey from the iD Editor. This means you don't need to create a custom survey before you can start making observations that can upload to OSM. To upload new surveys, locate the survey YAML file on your local hard drive. Find more information about how to create your own survey <a className='link--primary link--lowercase' href="https://github.com/osmlab/field-data-collection/blob/master/docs/surveys.md">here</a>.</p>
+          <p>The Observe App comes pre-filled with the standard OSM survey from the iD Editor. This means you don't need to create a custom survey before you can start making observations that can upload to OSM. To upload new surveys, locate the survey YAML file on your local hard drive. Find more information about how to create your own survey <a className='link--primary link--lowercase' data-href='https://github.com/osmlab/field-data-collection/blob/master/docs/surveys.md' onClick={openExternalLink}>here</a>.</p>
           <img alt='importing surveys' className='img-about' src='./assets/icons/about-surveys.jpg' />
 
-          <p>Once a survey and geographic area have been specified, the mobile app will be ready to use. Simply go to the Surveys tab and "Add a New Survey" in the mobile app to pull the information you've uploaded from the desktop app.</p>
+          <p>Once a survey and geographic area have been specified, the mobile app will be ready to use. Simply go to the Surveys tab and 'Add a New Survey' in the mobile app to pull the information you've uploaded from the desktop app.</p>
         </section>
         <section className='row section__about'>
           <h3>Receiving Data</h3>
-          <p>Once observations have been collected and mobile devices are on the same wifi network as the desktop application, press sync at the top of the "My Observations" page. This will sync all observations to the desktop app, to prevent data loss. Once pressed data will show up on the desktop application, ready to be manipulated and exported to OSM.</p>
+          <p>Once observations have been collected and mobile devices are on the same wifi network as the desktop application, press sync at the top of the 'My Observations' page. This will sync all observations to the desktop app, to prevent data loss. Once pressed data will show up on the desktop application, ready to be manipulated and exported to OSM.</p>
         </section>
       </div>
     )
   }
 }
 
-module.exports = connect()(About)
+module.exports = About
--- src/ui/surveys/Import.jsx
+++ src/ui/surveys/Import.jsx
@@ -3,8 +3,8 @@
 const React = require('react')
 const { connect } = require('react-redux')
 const { Link } = require('react-router-dom')
-
 const { importSurvey } = require('../../actions')
+const openExternalLink = require('../external-link')
 
 class Import extends React.Component {
   render () {
@@ -13,7 +13,7 @@ class Import extends React.Component {
     return (
       <div className='surveyInput'>
         <h3 className='withDescription'>Add new survey details</h3>
-        <p className='description'>Only surveys that are in a YAML format can be imported. More information on how to format or modify an existing YAML file can be found on the <Link to='/about' className='link--primary link--lowercase'>about page</Link> or <a className='link--primary link--lowercase' href="https://github.com/osmlab/field-data-collection/blob/master/docs/surveys.md">here</a>.</p>
+        <p className='description'>Only surveys that are in a YAML format can be imported. More information on how to format or modify an existing YAML file can be found on the <Link to='/about' className='link--primary link--lowercase'>about page</Link> or <a className='link--primary link--lowercase' data-href='https://github.com/osmlab/field-data-collection/blob/master/docs/surveys.md' onClick={openExternalLink}>here</a>.</p>
         <button className='button buttonGroup' onClick={importSurvey}>Import Existing Survey</button>
       </div>
     )
